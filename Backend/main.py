c2b5dc5bdeac03c6f2a5142f795d7175d1731f97
thibaultvanni <tibo.vanni@gmail.com>
2025-08-15 19:10:46 +0200

    First version

---
+++ Backend/main.py
@@ -0,0 +1,66 @@
+import threading
+from pyngrok import ngrok
+import uvicorn
+from fastapi import *
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from flask import Flask, render_template, request, jsonify
+import sqlite3
+from starlette.staticfiles import StaticFiles
+
+# This is all necessary to launch the app.
+# The app.mount is usefull for using all the elements in static files. In other words, usefull for using js file ect.
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+
+def updateDB(hostname, ip_adress):
+    """
+    Updating the Database of all the users that visited the website
+    :param hostname:
+    :param ip_adress:
+    :return: Updated DB
+    """
+    conn = sqlite3.connect("visitor.db")
+    cur = conn.cursor()
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS visitorJournal (
+        visitorNumber INTEGER PRIMARY KEY AUTOINCREMENT,
+        ip TEXT)
+    """)
+
+    cur.execute("INSERT INTO visitorJournal (ip) VALUES (?)", (ip_adress,))
+
+    cur.execute("SELECT * FROM visitorJournal WHERE ip = ?", (ip_adress,))
+    result = cur.fetchall()
+
+    if len(result) == 1:
+        phrase = "Hello " + hostname +". Welcome on this incredible journey in the coding world"
+    if len(result) > 1:
+        phrase = "Welcome back " + hostname + ". Happy to see you again."
+
+    conn.commit()
+    conn.close()
+    return(phrase, ip_adress)
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    client_ip = request.client.host
+    name = "TEST Name"
+    updateDB(name, client_ip)
+    return templates.TemplateResponse(
+        "homepage.html", {"request": request, "ip": client_ip}
+    )
+
+
+def start_ngrok():
+    public_url = ngrok.connect(8000, bind_tls=True)
+    print(f"🌍 Public URL: {public_url}")
+
+if __name__ == "__main__":
+    threading.Thread(target=start_ngrok).start()
+    # Lancer FastAPI
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ Backend/static/js/script.js
@@ -0,0 +1,20 @@
+console.log("External JS loaded!");
+
+
+function showMessage() {
+    const output = document.getElementById('output');
+    output.textContent = "Hello from external JS file!";
+}
+
+// Example: update clock every second
+function updateClock() {
+    const now = new Date();
+    document.getElementById('clock').textContent = now.toLocaleTimeString();
+}
+
+// Wait until DOM is loaded
+document.addEventListener('DOMContentLoaded', function() {
+    showMessage();
+    updateClock();
+    setInterval(updateClock, 1000);
+});
